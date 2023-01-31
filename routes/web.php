63342e8a3c7f1efb408264b3b56534ca53deae03
ctriyanti <riyanti@cashtree.id>
2023-01-31 17:40:48 +0700

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Guest Page
 Route::get('/about', function () {
     return view('guest.about');
 });
 
 Route::get('/contact', function () {
     return view('guest.contact');
-});
-
-// Route::get('/article', function () {
-//     return view('guest.article');
-// });
-
-// Route::get('/article-detail', function () {
-//     return view('guest.articleDetail');
-// });
-
-Route::get('/admin/dashboard', function () {
-    return view('admin.index');
 });
 
 // User Page
@@ -50,51 +39,52 @@ Route::post('/subscribe', [GeneralController::class, 'subscribe'])->name('subscr
 Route::get('/send-mail', [MailController::class, 'send_mail']);
 
 
-// Banner
-Route::get('/admin/banners', [BannerController::class, 'index'])->name('banners');
-Route::get('/admin/banner/create', [BannerController::class, 'create']);
-Route::post('/admin/banner/create', [BannerController::class, 'submit'])->name('submitBanner');
-Route::get('/admin/banner/edit/{id}', [BannerController::class, 'edit']);
-Route::post('/admin/banner/edit', [BannerController::class, 'update'])->name('updateBanner');
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.index');
+    });
+    // Banner
+    Route::get('/admin/banners', [BannerController::class, 'index'])->name('banners');
+    Route::get('/admin/banner/create', [BannerController::class, 'create']);
+    Route::post('/admin/banner/create', [BannerController::class, 'submit'])->name('submitBanner');
+    Route::get('/admin/banner/edit/{id}', [BannerController::class, 'edit']);
+    Route::post('/admin/banner/edit', [BannerController::class, 'update'])->name('updateBanner');
 
-// Article
-Route::get('/admin/articles', [ArticleController::class, 'index'])->name('articles');
-Route::get('/admin/article/create', [ArticleController::class, 'create']);
-Route::post('/admin/article/create', [ArticleController::class, 'submit'])->name('submitArticle');
-Route::get('/admin/article/edit/{id}', [ArticleController::class, 'edit']);
-Route::post('/admin/article/edit', [ArticleController::class, 'update'])->name('updateArticle');
+    // Article
+    Route::get('/admin/articles', [ArticleController::class, 'index'])->name('articles');
+    Route::get('/admin/article/create', [ArticleController::class, 'create']);
+    Route::post('/admin/article/create', [ArticleController::class, 'submit'])->name('submitArticle');
+    Route::get('/admin/article/edit/{id}', [ArticleController::class, 'edit']);
+    Route::post('/admin/article/edit', [ArticleController::class, 'update'])->name('updateArticle');
 
-// Primary Article
-Route::get('/admin/primary-article', [ArticleController::class, 'primary_article'])->name('primaryArticle');
-Route::get('/admin/primary-article/create', [ArticleController::class, 'primary_create']);
-Route::post('/admin/primary-article/create', [ArticleController::class, 'primary_submit'])->name('submitPrimary');
+    // Primary Article
+    Route::get('/admin/primary-article', [ArticleController::class, 'primary_article'])->name('primaryArticle');
+    Route::get('/admin/primary-article/create', [ArticleController::class, 'primary_create']);
+    Route::post('/admin/primary-article/create', [ArticleController::class, 'primary_submit'])->name('submitPrimary');
 
-// Offer
-Route::get('/admin/offer', [OfferController::class, 'index'])->name('offer');
-Route::get('/admin/offer/edit/{id}', [OfferController::class, 'edit']);
-Route::post('/admin/offer/edit/', [OfferController::class, 'update'])->name('updateOffer');
+    // Offer
+    Route::get('/admin/offer', [OfferController::class, 'index'])->name('offer');
+    Route::get('/admin/offer/edit/{id}', [OfferController::class, 'edit']);
+    Route::post('/admin/offer/edit/', [OfferController::class, 'update'])->name('updateOffer');
 
-// Client
-Route::get('/admin/clients', [ClientController::class, 'index'])->name('client');
-Route::get('/admin/client/create', [ClientController::class, 'create']);
-Route::post('/admin/client/create', [ClientController::class, 'submit'])->name('submitClient');
-Route::get('/admin/client/edit/{id}', [ClientController::class, 'edit']);
-Route::post('/admin/client/edit', [ClientController::class, 'update'])->name('updateClient');
+    // Client
+    Route::get('/admin/clients', [ClientController::class, 'index'])->name('client');
+    Route::get('/admin/client/create', [ClientController::class, 'create']);
+    Route::post('/admin/client/create', [ClientController::class, 'submit'])->name('submitClient');
+    Route::get('/admin/client/edit/{id}', [ClientController::class, 'edit']);
+    Route::post('/admin/client/edit', [ClientController::class, 'update'])->name('updateClient');
 
-// Testimonial
-Route::get('/admin/testimonials', [TestimonialController::class, 'index'])->name('testimonials');
-Route::get('/admin/testimonial/create', [TestimonialController::class, 'create']);
-Route::post('/admin/testimonial/create', [TestimonialController::class, 'submit'])->name('addTestimoni');
-Route::get('/admin/testimonial/edit/{id}', [TestimonialController::class, 'edit']);
-Route::post('/admin/testimonial/edit', [TestimonialController::class, 'update'])->name('updateTestimoni');
+    // Testimonial
+    Route::get('/admin/testimonials', [TestimonialController::class, 'index'])->name('testimonials');
+    Route::get('/admin/testimonial/create', [TestimonialController::class, 'create']);
+    Route::post('/admin/testimonial/create', [TestimonialController::class, 'submit'])->name('addTestimoni');
+    Route::get('/admin/testimonial/edit/{id}', [TestimonialController::class, 'edit']);
+    Route::post('/admin/testimonial/edit', [TestimonialController::class, 'update'])->name('updateTestimoni');
 
-// Subscribe
-Route::get('/admin/subscribe', [SubscribeController::class, 'index'])->name('testimonials');
+    // Subscribe
+    Route::get('/admin/subscribe', [SubscribeController::class, 'index'])->name('testimonials');
 
-
-
-
-
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
