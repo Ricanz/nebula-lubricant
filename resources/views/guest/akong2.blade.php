57f04760cc5e6c637b78d4dc26fcc439480b0851
ctriyanti <riyanti@cashtree.id>
2023-03-30 12:26:01 +0700

    google maps color

--- resources/views/guest/akong2.blade.php
+++ resources/views/guest/akong2.blade.php
@@ -227,6 +227,7 @@
                                     <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.052487339406!2d106.62096421476872!3d-6.1236387955660945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4be4c3e6021e8dc9!2zNsKwMDcnMjUuMSJTIDEwNsKwMzcnMjMuNCJF!5e0!3m2!1sid!2sid!4v1675140128233!5m2!1sid!2sid"
                                         class="google-map__one" allowfullscreen></iframe>
+                                        {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7934.104971641613!2d106.623167!3d-6.123639!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a01f35a280c85%3A0xbe67e04ef16c91ab!2sVihara%20Araya%20Dhamma!5e0!3m2!1sid!2sid!4v1680153833025!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> --}}
                                 </div>
                             </div>
                         </div>
